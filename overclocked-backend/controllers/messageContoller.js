54e0c0155f11f9509f3a29ae9f76cfea545cad06
Ammar Yasser <48571369+akilany@users.noreply.github.com>
2022-03-11 15:53:10 +0200

    Delete overclocked-backend directory

--- overclocked-backend/controllers/messageContoller.js
+++
@@ -1,9 +0,0 @@
-const Message = require('../models/messageModel')
-const factory = require('./handlerFactory')
-
-// Routes Handler
-exports.getMessages = factory.getAll(Message)
-exports.getSingleMessage = factory.getOne(Message)
-exports.createMessage = factory.createOne(Message)
-exports.updateMessage = factory.updateOne(Message)
-exports.deleteMessage = factory.deleteOne(Message)
